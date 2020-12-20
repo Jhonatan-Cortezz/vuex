4cc9bc356cd30ff50145f7531e33468d76612dfe
Jhonatan Cortez <jhonatancortez708@gmail.com>
2020-12-20 15:57:01 -0600

    Iniciando formularios vue (#1 navabar)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar navbar-dark bg-dark">
+    <router-link to="/" class="navbar-brand">
+      Inicio
+    </router-link>
+    <router-link to="/about" class="navbar-brand">
+      About
+    </router-link>
+    <router-link to="/formulario" class="navbar-brand">
+      Formularios
+    </router-link>
+
+    <div class="d-flex">
+      <router-link class="btn btn-dark" to="/">
+        Tareas
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
