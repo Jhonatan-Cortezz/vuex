1c8d7de432fe9bbc1e5543f41994899d520fcfe6
Jhonatan Cortez <jhonatancortez708@gmail.com>
2020-12-20 14:03:56 -0600

    Comunicacion entre input y data

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,18 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h1>Mi contador segunda vista: {{ $store.state.contador}}</h1>
+
+    <input type="text" name="" id="" v-model="texto"> <!-- v-model me permite comunicar el input con componentes -->
+    <h1>{{texto}}</h1>
+
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      texto: ''
+    }
+  },
+}
+</script>
