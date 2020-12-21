1b672b89b328a7be101d03072784f4377788b487
Jhonatan Cortez <jhonatancortez708@gmail.com>
2020-12-20 20:29:34 -0600

    Iniciando crud localStorage - separacion de form en components

--- src/views/formulario.vue
+++ src/views/formulario.vue
@@ -1,54 +1,24 @@
 <template>
   <form @submit.prevent="procesarFormulario">
-    <input type="text" 
-      name="" 
-      id="" 
-      class="form-control my-2" 
-      placeholder="Ingrese el nombre"
-      v-model.trim="tarea.nombre"><!-- trim limpia los espacios -->
-
-    <div class="form-check form-check-inline">
-      <input type="checkbox" class="form-check-input" v-model="tarea.categorias" id="check-1" value="Javascript">
-      <label for="check-1" class="form-check-label">Javascript</label>
-    </div>
-
-    <div class="form-check form-check-inline">
-      <input type="checkbox" class="form-check-input" id="check-2" v-model="tarea.categorias" value="Node.js">
-      <label for="check-2" class="form-check-label">Node.js</label>
-    </div>
-
-    <div class="mt-2">
-      <div class="form-check form-check-inline">
-        <input type="radio" class="form-check-input" id="rd-1" v-model="tarea.estado" value="Urgente">
-        <label for="rd-1" class="form-check-label">Urgente</label>
-      </div>
-
-      <div class="form-check form-check-inline">
-        <input type="radio" class="form-check-input" id="rd-2" v-model="tarea.estado" value="Relax">
-        <label for="rd-2" class="form-check-label">Relax</label>
-      </div>
-    </div>
-
-    <!-- Para los input tipo numbre -->
-    <div class="mt-2">
-      <div class="">
-        <input type="number" id="" v-model.number="tarea.numero"><!-- here a diferent -->
-      </div>
-    </div>
-
-    <button class="btn btn-dark mt-2 btn-block" type="submit" :disabled="bloquear">
-      Procesar
-    </button>
+    <Input :tarea="tarea"/>
   </form>
   <hr>
   <p>{{tarea}}</p>
 </template>
 
 <script>
+
+import Input from '../components/Input'
+
 export default {
+  components: {
+    Input
+  },
+
   data() {
     return {
       tarea: {
+        id: '',
         nombre: '',
         categorias: [],
         estado: '',
@@ -74,12 +44,6 @@ export default {
         estado: '',
         numero: 0
       }
-    }
-  },
-
-  computed: {
-    bloquear() {
-      return this.tarea.nombre.trim() === "" ? true : false
     }
   },
 }
